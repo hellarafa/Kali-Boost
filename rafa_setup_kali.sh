3f0c774728f15ab2620f2459e71d6906cc275f6b
hellarafa <freefastfire@gmail.com>
2018-04-12 10:47:38 -0700

    Working on skript.

--- rafa_setup_kali.sh
+++ rafa_setup_kali.sh
@@ -20,44 +20,44 @@ test() {
 cme_bleeding_edge() {
 	echo -e $green"Installing: CME Bleeding-Edge"$reset
 	    if [ ! -d "CrackMapExec" ]; then
-		    git clone https://github.com/byt3bl33d3r/CrackMapExec.git
-		    apt-get install -y libssl-dev libffi-dev python-dev build-essential
-		    ret="$?"
+            git clone https://github.com/byt3bl33d3r/CrackMapExec.git
+            apt-get install -y libssl-dev libffi-dev python-dev build-essential
+            ret="$?"
             (cd /opt/CrackMapExec && git submodule init && git submodule update --recursive)
-		    (cd /opt/CrackMapExec && python setup.py install)
-		    success "Successfully installed CrackMapExec Bleeding Edge"
-	    else
+            (cd /opt/CrackMapExec && python setup.py install)
+            success "Successfully installed CrackMapExec Bleeding Edge"
+	        debug
+        else
             skipmsg "Found CrackMapExec Bleeding-Edge in /opt. Skipping installation."
 	    fi
-    debug
 	sleep 1
 }
 
 empire() {
 	echo -e $green"Installing: Empire"$reset
 	    if [ -d "Empire" ]; then
-		    skipmsg "Found Empire in /opt. Skipping installation."
-		    read -p "Do you want to update instead (y/n)? " choice2
-		    case "$choice2" in
-			    [yY]|[yYeEsS] ) (cd /opt/Empire && git pull)
+            skipmsg "Found Empire in /opt. Skipping installation."
+            read -p "Do you want to update instead (y/n)? " choice2
+            case "$choice2" in
+                [yY]|[yYeEsS] ) (cd /opt/Empire && git pull)
                             ret="$?"
                             success "Successfully updated Empire."
                             ;;
-			    [nN]|[nNoO] ) skipmsg "Not updating Empire.";;
-			* ) skipmsg "Not updating Empire.";;
-		    esac
+                [nN]|[nNoO] ) skipmsg "Not updating Empire.";;
+                * ) skipmsg "Not updating Empire.";;
+            esac
 	    fi
 	    if [ ! -d "Empire" ]; then
-		    git clone https://github.com/EmpireProject/Empire.git
-		    read -p "Empire requires extra installation steps. Do you want to continue with the Empire install? (y/n)? " choice1
-		    case "$choice1" in
-			    [yY]|[yYeEsS] ) (cd Empire && ./setup/install.sh)
+            git clone https://github.com/EmpireProject/Empire.git
+            read -p "Empire requires extra installation steps. Do you want to continue with the Empire install? (y/n)? " choice1
+            case "$choice1" in
+                [yY]|[yYeEsS] ) (cd Empire && ./setup/install.sh)
                             ret="$?"
                             success "Successfully installed Empire."
                             ;;
-			   [nN]|[nNoO] ) skipmsg "Skipping installation. Manually run install.sh in the EmpireProject folder to install.";;
-			* ) skipmsg "Skipping installation. Manually run install.sh in the EmpireProject folder to install.";;
-		    esac
+                [nN]|[nNoO] ) skipmsg "Skipping installation. Manually run install.sh in the EmpireProject folder to install.";;
+                * ) skipmsg "Skipping installation. Manually run install.sh in the EmpireProject folder to install.";;
+            esac
 	    fi
         debug
 	sleep 1
@@ -66,12 +66,12 @@ empire() {
 firefox_password_cracker() {
 	echo -e $green"Installing: ffpasscracker"$reset
 	    if [ ! -d "ffpasscracker" ]; then
-		    git clone https://github.com/pradeep1288/ffpasscracker.git
-	        ret="$?"
+            git clone https://github.com/pradeep1288/ffpasscracker.git
+            ret="$?"
             success "Successfully installed ffpasscracker."
             debug
         else
-		    skipmsg "Found ffpasscracker script in /opt. Skipping installation."
+            skipmsg "Found ffpasscracker script in /opt. Skipping installation."
 	    fi
 	sleep 1
 }
@@ -79,13 +79,13 @@ firefox_password_cracker() {
 eyewitness() {
 	echo -e $green"Installing: EyeWitness"$reset
 	    if [ ! -d "EyeWitness" ]; then
-		    git clone https://github.com/ChrisTruncer/EyeWitness.git
-		    (cd /opt/EyeWitness/setup/ && ./setup.sh )
-	        ret="$?"
+            git clone https://github.com/ChrisTruncer/EyeWitness.git
+            (cd /opt/EyeWitness/setup/ && ./setup.sh )
+            ret="$?"
             success "Successfully installed EyeWitness."
             debug
         else
-		    skipmsg "Found EyeWitness in /opt. Skipping installation."
+            skipmsg "Found EyeWitness in /opt. Skipping installation."
 	    fi
 	sleep 1
 }
@@ -93,13 +93,13 @@ eyewitness() {
 impacket_tools() {
 	echo -e $green"Installing: Impacket"$reset
 	    if [ ! -d "impacket" ]; then
-		    git clone https://github.com/CoreSecurity/impacket.git
-		    (cd impacket && python setup.py install)
-	        ret="$?"
+            git clone https://github.com/CoreSecurity/impacket.git
+            (cd impacket && python setup.py install)
+            ret="$?"
             success "Successfully installed Impacket."
             debug
         else
-		   skipmsg "Found impacket in /opt. Skipping installation."
+            skipmsg "Found impacket in /opt. Skipping installation."
 	    fi
 	sleep 1
 }
@@ -133,9 +133,12 @@ mimipenguin() {
 noSQLMap() {
 	echo -e $green"Installing: NoSQLMap"$reset
 	    if [ ! -d "NoSQLMap" ]; then
-		git clone https://github.com/codingo/NoSQLMap.git
-	    else
-		echo -e $red"Found mimipenguin in /opt. Skipping..."$reset
+		    git clone https://github.com/codingo/NoSQLMap.git
+	        ret="$?"
+            success "Successfully installed NoSQLMap."
+            debug
+        else
+	        skipmsg "Found mimipenguin in /opt. Skipping installation."
 	    fi
 	sleep 1
 }
@@ -143,11 +146,14 @@ noSQLMap() {
 masscan() {
 	echo -e $green"Installing: Masscan"$reset
 	    if [ ! -d "masscan" ]; then
-		git clone https://github.com/robertdavidgraham/masscan.git
-		apt-get install -y git gcc make libpcap-dev
-		(cd /opt/masscan && make -j && make install)
-	    else
-		echo -e $red"Found Masscan in /opt. Skipping..."$reset
+            git clone https://github.com/robertdavidgraham/masscan.git
+            apt-get install -y git gcc make libpcap-dev
+            (cd /opt/masscan && make -j && make install)
+	        ret="$?"
+            success "Successfully installed Masscan."
+            debug
+        else
+            skipmsg "Found Masscan in /opt. Skipping installation."
 	    fi
 	sleep 1
 }
@@ -155,9 +161,12 @@ masscan() {
 powershellPopup() {
 	echo -e $green"Installing: PowerShell_Popup"$reset
 	    if [ ! -d "PowerShell_Popup" ]; then
-		git clone https://github.com/cheetz/PowerShell_Popup.git
-	    else
-		echo -e $red"Found PowerShell Popup in /opt. Skipping..."$reset
+	        git clone https://github.com/cheetz/PowerShell_Popup.git
+	        ret="$?"
+            success "Successfully installed PowerShell_Popup."
+            debug
+        else
+	        skipmsg "Found PowerShell Popup in /opt. Skipping installation."
 	    fi
 	sleep 1
 }
@@ -165,9 +174,12 @@ powershellPopup() {
 powersploit() {
 	echo -e $green"Installing: PowerSploit"$reset
 	    if [ ! -d "PowerSploit" ]; then
-		git clone https://github.com/PowerShellMafia/PowerSploit.git
-	    else
-		echo -e $red"Found PowerSploit in /opt. Skipping..."$reset
+	        git clone https://github.com/PowerShellMafia/PowerSploit.git
+	        ret="$?"
+            success "Successfully installed PowerSploit."
+            debug
+        else
+	        skipmsg "Found PowerSploit in /opt. Skipping installation."
 	    fi
 	sleep 1
 }
@@ -175,9 +187,12 @@ powersploit() {
 firefox_password_cracker2() {
 	echo -e $green"Installing: python-ffpassdecrypt"$reset
 	    if [ ! -d "python-ffpassdecrypt" ]; then
-		git clone https://github.com/nyov/python-ffpassdecrypt.git
+            git clone https://github.com/nyov/python-ffpassdecrypt.git
+            ret="$?"
+            success "Successfully installed python-ffpassdecrypt."
+            debug
 	    else
-		echo -e $red"Found python-ffpassdecrypt in /opt. Skipping..."$reset
+            skipmsg "Found python-ffpassdecrypt in /opt. Skipping installation."
 	    fi
 	sleep 1
 }
@@ -185,10 +200,13 @@ firefox_password_cracker2() {
 rtfm() {
 	echo -e $green"Installing: rtfm"$reset
 	    if [ ! -d "rtfm" ]; then
-		git clone https://github.com/leostat/rtfm.git
-		chmod +x /opt/rtfm/rtfm.py
+	        git clone https://github.com/leostat/rtfm.git
+            ret="$?"
+            success "Successfully installed rtfm."
+            debug
+	        chmod +x /opt/rtfm/rtfm.py
 	    else
-		echo "Nice. You got the red team field manual."
+	        skipmsg "Found rftm in /opt. Skipping installation."
 	    fi
 	sleep 1
 }
@@ -196,9 +214,12 @@ rtfm() {
 seclists() {
 	echo -e $green"Installing: SecLists"$reset
 	    if [ ! -d "SecLists" ]; then
-		git clone https://github.com/danielmiessler/SecLists.git
-	    else
-		echo -e $red"Found Seclists in /opt. Skipping..."$reset
+	        git clone https://github.com/danielmiessler/SecLists.git
+	        ret="$?"
+            success "Successfully installed SecLists."
+            debug
+        else
+	        skipmsg "Found Seclists in /opt. Skipping installation."
 	    fi
 	sleep 1
 }
@@ -206,9 +227,12 @@ seclists() {
 wifite() {
 	echo -e $green"Installing: wifite"$reset
 	    if [ ! -d "wifite" ]; then
-		git clone https://github.com/derv82/wifite.git
-	    else
-		echo -e $red"Found wifite in /opt. Skipping..."$reset
+	        git clone https://github.com/derv82/wifite.git
+	        ret="$?"
+            success "Successfully installed wifite."
+            debug
+        else
+	        skipmsg "Found wifite in /opt. Skipping installation."
 	    fi
 	sleep 1
 }
@@ -216,9 +240,12 @@ wifite() {
 wifite2() {
 	echo -e $green"Installing: wifite2"$reset
 	    if [ ! -d "wifite2" ]; then
-		git clone https://github.com/derv82/wifite2.git
-	    else
-		echo -e $red"Found wifite2 in /opt. Skipping..."$reset
+	        git clone https://github.com/derv82/wifite2.git
+	        ret="$?"
+            success "Successfully installed wifite2."
+            debug
+        else
+	        skipmsg "Found wifite2 in /opt. Skipping installation."
 	    fi
 	sleep 1
 }
@@ -226,19 +253,12 @@ wifite2() {
 nishang() {
 	echo -e $green"Installing: nishang"$reset
 	    if [ ! -d "./nishang/" ]; then
-		git clone https://github.com/samratashok/nishang.git 
-	    else
-		echo -e $red"Found nishang in /opt. Skipping..."$reset
-	    fi
-	sleep 1
-}
-
-powershell() {
-	echo -e $green"Installing: PowerShell"$reset
-	    if [ ! -d "PowerShell" ]; then
-		git clone https://github.com/PowerShell/PowerShell.git
-	    else
-		echo -e $red"Found PowerShell in /opt. Skipping..."$reset
+	        git clone https://github.com/samratashok/nishang.git 
+	        ret="$?"
+            success "Successfully installed nishang."
+            debug
+        else
+	        skipmsg "Found nishang in /opt. Skipping installation."
 	    fi
 	sleep 1
 }
@@ -246,9 +266,12 @@ powershell() {
 worawitms17() {
 	echo -e $green"Installing: Worawit's MS17-010"$reset
 	    if [ ! -d "MS17-010" ]; then
-		git clone https://github.com/worawit/MS17-010.git
-	    else
-		echo -e $red"Found MS17-010 in /opt. Skipping..."$reset
+            git clone https://github.com/worawit/MS17-010.git
+	        ret="$?"
+            success "Successfully installed Worawit's MS17-010."
+            debug
+        else
+	        skipmsg "Found MS17-010 in /opt. Skipping installation."
 	    fi
 	sleep 1
 }
@@ -256,10 +279,13 @@ worawitms17() {
 vulscan() {
 	echo -e $green"Installing: Vulscan"$reset
 	    if [ ! -d "vulscan" ]; then
-		git clone https://github.com/scipag/vulscan.git
-		cp -r vulscan/ /usr/share/nmap/scripts/
-	    else
-		echo -e $red"Found vulscan in /opt. Skipping..."$reset
+            git clone https://github.com/scipag/vulscan.git
+	        ret="$?"
+            cp -r vulscan/ /usr/share/nmap/scripts/
+	        success "Successfully installed Vulscan. Find it in your nmap scripts."
+            debug
+        else
+	        skipmsg "Found vulscan in /opt. Skipping installation."
 	    fi
 	sleep 1
 }
@@ -267,10 +293,13 @@ vulscan() {
 vulners() {
 	echo -e $green"Installing: Vulners"$reset
 	    if [ ! -d "nmap-vulners" ]; then
-		git clone https://github.com/vulnersCom/nmap-vulners.git
-		cp nmap-vulners/vulners.nse /usr/share/nmap/scripts/
-	    else
-		echo -e $red"Found Vulners Nmap Script in /opt. Skipping..."$reset
+            git clone https://github.com/vulnersCom/nmap-vulners.git
+            ret="$?"
+	        cp nmap-vulners/vulners.nse /usr/share/nmap/scripts/
+	        success "Successfully installed Vulners. Find it in your nmap scripts."
+            debug
+        else
+	        skipmsg "Found Vulners Nmap Script in /opt. Skipping installation."
 	    fi
 	sleep 1
 }
@@ -278,9 +307,12 @@ vulners() {
 credninja() {
 	echo -e $green"Installing: CredNinja"$reset
 	    if [ ! -d "CredNinja" ]; then
-		git clone https://github.com/Raikia/CredNinja.git
-	    else
-		echo -e $red"Found CredNinja in /opt. Skipping..."$reset
+            git clone https://github.com/Raikia/CredNinja.git
+	        ret="$?"
+            success "Successfully installed CredNinja."
+            debug
+        else
+	        skipmsg "Found CredNinja in /opt. Skipping installation."
 	    fi
 	sleep 1
 }
@@ -288,9 +320,12 @@ credninja() {
 elfStrings() {
 	echo -e $green"Installing: ELF-Strings"$reset
 	    if [ ! -d "elf-strings" ]; then
-		git clone https://github.com/LloydLabs/elf-strings
+	        git clone https://github.com/LloydLabs/elf-strings
+            ret="$?"
+            success "Successfully installed ELF-Strings."
+            debug
 	    else
-		echo -e $red"Found ELF-Strings in /opt. Skipping..."$reset
+	        skipmsg "Found ELF-Strings in /opt. Skipping installation."
 	    fi
 	sleep 1
 }
@@ -298,9 +333,12 @@ elfStrings() {
 dotdotslash() {
 	echo -e $green"Installing: dotdotslash"$reset
 	    if [ ! -d "dotdotslash" ]; then
-		git clone https://github.com/jcesarstef/dotdotslash.git
-	    else
-		echo -e $red"Found dotdotslash in /opt. Skipping..."$reset
+	        git clone https://github.com/jcesarstef/dotdotslash.git
+	        ret="$?"
+            success "Successfully installed dotdotslash."
+            debug
+        else
+            skipmsg "Found dotdotslash in /opt. Skipping installation."
 	    fi
 	sleep 1
 }
@@ -308,11 +346,13 @@ dotdotslash() {
 burp_vulners() {
 	echo -e $green"Installing: burp-vulners-scanner"$reset
 	    if [ ! -d "burp-vulners-scanner" ]; then
-		git clone https://github.com/vulnersCom/burp-vulners-scanner.git
-		echo "Don't forget to manually install this module in Burp."
-		sleep 3
+	        git clone https://github.com/vulnersCom/burp-vulners-scanner.git
+            ret="$?"
+            success "Successfully downloaded burp-vulners-scanner."
+	        echo -e $lightYellow"Don't forget to manually install this module in Burp."$reset
+	        sleep 3
 	    else
-		echo -e $red"Found burp-vulners-scanner in /opt. Skipping..."$reset
+	        skipmsg "Found burp-vulners-scanner in /opt. Skipping installation."
 	    fi
 	sleep 1
 }
@@ -320,9 +360,12 @@ burp_vulners() {
 whonow() {
 	echo -e $green"Installing: whonow"$reset
 	    if [ ! -d "whonow" ]; then
-		git clone https://github.com/brannondorsey/whonow.git
-	    else
-		echo -e $red"Found whonow in /opt. Skipping..."$reset
+	        git clone https://github.com/brannondorsey/whonow.git
+	        ret="$?"
+            success "Successfully installed whonow."
+            debug
+        else
+	        skipmsg "Found whonow in /opt. Skipping installation."
 	    fi
 	sleep 1
 }
@@ -330,11 +373,27 @@ whonow() {
 wfuzz() {
 	echo -e $green"Installing: wfuzz"$reset
 	    if [ ! -d "wfuzz" ]; then
-	       git clone https://github.com/xmendez/wfuzz.git
-	    else
-		echo -e $red"Found wfuzz in /opt. Skipping..."$reset
+	        git clone https://github.com/xmendez/wfuzz.git
+	        ret="$?"
+            success "Successfully installed wfuzz."
+            debug
+        else
+	        skipmsg "Found wfuzz in /opt. Skipping installation."
 	    fi
 	sleep 1
+}
+
+probableWordlists(){
+    echo -e $green"Installing: Probable-Wordlists"$reset
+        if [ ! -d "Probable-Wordlists" ]; then
+            git clone https://github.com/berzerk0/Probable-Wordlists.git
+            ret="$?"
+            success "Successfully installed Probable-Wordlists."
+            debug
+        else
+            skipmsg "Found Probable-Wordlists in /opt. Skipping installation."
+        fi
+    sleep 1
 }
 
 #################### REDTEAM EVIL STUFF
@@ -401,51 +460,6 @@ banner() {
     echo ""
 }
 
-#################### CALL TABLE & START
-#  This is where the program starts. I call the section that follows the call table. If you 
-#  comment out a line, that program does not get installed. Simple as that. As a default, all
-#  tools are installed except the REDTEAM EVIL STUFF. You can install it if you'd like.
-
-start() {
-    banner
-    cme_bleeding_edge #1
-    empire #2
-    firefox_password_cracker #3
-    eyewitness #4
-    impacket_tools #5
-    mimikittenz #6
-    mimipenguin #7
-    noSQLMap #8
-    masscan #9
-    powershellPopup #10
-    powersploit #11
-    firefox_password_cracker2 #12
-    rtfm #13
-    seclists #14
-    wifite #15
-    wifite2 #16
-    nishang #17
-    powershell #18
-    worawitms17 #19
-    vulscan #20
-    vulners #21
-    credninja #22
-    elfStrings #23
-    dotdotslash #24
-    burp_vulners #25
-    whonow #26
-    wfuzz #27 
-
-    #ADD
-    #mimikatz
-    #peterkim's tools
-    #https://github.com/gentilkiwi/mimikatz/releases
-
-    #RED
-    #windowsHacks
-    echo -e "\n DONE! Thanks. Come again!"
-}
-
 #################### PROGRAM FUNCTIONS
 # Functions that make the program run nicer.
 #
@@ -475,7 +489,6 @@ debug() {
     fi
 }
 
-
 #################### SETUP PARAMETERS
 # Just all the setup parameters, variables and stuff.
 #
@@ -491,6 +504,51 @@ readonly lightCyan="\e[96m"
 #readonly cyan="\e[36m"
 #readonly lightBlue="\e[94m"
 #readonly lightMagenta="\e[95m"
+
+#################### CALL TABLE & START
+#  This is where the program starts. I call the section that follows the call table. If you 
+#  comment out a line, that program does not get installed. Simple as that. As a default, all
+#  tools are installed except the REDTEAM EVIL STUFF. You can install it if you'd like.
+
+start() {
+    banner
+    cme_bleeding_edge #1
+    empire #2
+    firefox_password_cracker #3
+    eyewitness #4
+    impacket_tools #5
+    mimikittenz #6
+    mimipenguin #7
+    noSQLMap #8
+    masscan #9
+    powershellPopup #10
+    powersploit #11
+    firefox_password_cracker2 #12
+    rtfm #13
+    seclists #14
+    wifite #15
+    wifite2 #16
+    nishang #17
+    worawitms17 #18
+    vulscan #19
+    vulners #20
+    credninja #21
+    elfStrings #22
+    dotdotslash #23
+    burp_vulners #24
+    whonow #25
+    wfuzz #26
+    probableWordlists #27
+
+    #ADD
+    #mimikatz
+    #peterkim's tools
+    #https://github.com/gentilkiwi/mimikatz/releases
+
+    #RED
+    #windowsHacks
+    echo -e "\n DONE! Thanks. Come again!"
+}
 
 cd /opt
 #start
